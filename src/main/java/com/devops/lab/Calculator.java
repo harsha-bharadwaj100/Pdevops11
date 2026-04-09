9f3d56ee6a7860da8949713c954154378dafcf31
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2026-04-09 10:16:21 +0530

    Update output message in Calculator main method

--- src/main/java/com/devops/lab/Calculator.java
+++ src/main/java/com/devops/lab/Calculator.java
@@ -3,7 +3,7 @@ package com.devops.lab;
 public class Calculator {
     // DO NOT FORGET THIS FOR THE EXAM
     public static void main(String[] args) {
-        System.out.println("Docker is running the Calculator!");
+        System.out.println("Running the Calculator!");
     }
     
     public int multiply(int a, int b) { return a * b; }
